d53dacd8461bb00f1fd72f0609eeefd30adb6637
CrioUser <crio-user@criodo.com>
2022-10-22 00:13:26 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -1,82 +1,44 @@
 import config from "../conf/index.js";
 
 async function init() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  let cities = await fetchCities();
-
-  // const loadAnim = document.getElementById('load');
-  // loadAnim.remove();
-  
-=======
-=======
->>>>>>> 04a35f749ba52a552d9f517016c4c27857cb4e5f
   //Fetches list of all cities along with their images and description
   let cities = await fetchCities();
 
   //Updates the DOM with the cities
-<<<<<<< HEAD
->>>>>>> 04a35f749ba52a552d9f517016c4c27857cb4e5f
-=======
->>>>>>> 04a35f749ba52a552d9f517016c4c27857cb4e5f
   cities.forEach((key) => {
     addCityToDOM(key.id, key.city, key.description, key.image);
   });
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-async function fetchCities() {
-  try {
-    let res = await fetch(config.backendEndpoint + "/cities");
-    let data = res.json();
-    return data;
-  } catch (err) {
-    return null;
-  }
-}
-
-function addCityToDOM(id, city, description, image) {
-  let parent = document.getElementById("data");
-  let cityDiv = document.createElement("div");
-  cityDiv.classList.add("col-lg-3", "col-12", "col-sm-6", "mb-4");
-  cityDiv.innerHTML = `
-  <a href="/pages/adventures/?city=${id}" id="${id}">
-    <div class="tile">
-      
-        <img src="${image}" alt="City Image">
-      
-      <div class="tile-text text-center">
-        <h5>${city}</h5>
-        <p>${description}</p>
-      </div>
-    </div>
-  </a>
-  `;
-
-  parent.appendChild(cityDiv);
-}
-
-export { init, fetchCities, addCityToDOM };
-=======
-=======
->>>>>>> 04a35f749ba52a552d9f517016c4c27857cb4e5f
 //Implementation of fetch call
 async function fetchCities() {
-  // TODO: MODULE_CITIES
-  // 1. Fetch cities using the Backend API and return the data
-
+  try {
+    let response = await fetch(config.backendEndpoint + "/cities");
+    let citiesData = await response.json();
+    return citiesData;
+  } catch (error) {
+    console.log(`Error is : ${error}`);
+    return null;
+  }
 }
 
 //Implementation of DOM manipulation to add cities
 function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
-
+  let dataElem = document.getElementById("data");
+  dataElem.innerHTML += `<div class="col-6 col-lg-3 mb-4">
+  <a href="pages/adventures/?city=${id}" id="${id}">
+      <div class="tile text-white">
+      <div class="text-center tile-text">
+        <h5>${city}</h5>
+        <p>${description}</p>
+      </div>
+      <img src="${image}" alt="">
+    </div>
+  </a>
+</div>`;
+  // 1. Populate the City details and insert those details into the DOM
 }
 
 export { init, fetchCities, addCityToDOM };
-<<<<<<< HEAD
->>>>>>> 04a35f749ba52a552d9f517016c4c27857cb4e5f
-=======
->>>>>>> 04a35f749ba52a552d9f517016c4c27857cb4e5f
